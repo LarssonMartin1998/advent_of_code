220127437dbf7a86c1d7be77e01ecded16020599
Martin Larsson <larssonmartin1998@gmail.com>
2023-12-03 10:16:53 +0100

    Improve error handling in day one, now we just need to fix that dirty trick with the string matching :)))))

--- 2023/aoc_2023_1/src/main.rs
+++ 2023/aoc_2023_1/src/main.rs
@@ -1,45 +1,49 @@
 use std::fs::File;
-use std::io::{prelude::*, BufReader};
+use std::i32;
+use std::io::{self, prelude::*, BufReader};
 use std::str::Chars;
 
-fn get_first_num_in_string(input: &str) -> char {
+fn get_first_num_in_string(input: &str) -> Result<i32, io::Error> {
     let number_strings = [
-        ("one", '1'),
-        ("two", '2'),
-        ("three", '3'),
-        ("four", '4'),
-        ("five", '5'),
-        ("six", '6'),
-        ("seven", '7'),
-        ("eight", '8'),
-        ("nine", '9'),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
     ];
 
     get_first_number_in_iterator(input.chars(), &number_strings)
 }
 
-fn get_last_num_in_string(input: &str) -> char {
+fn get_last_num_in_string(input: &str) -> Result<i32, io::Error> {
     let number_strings = [
-        ("eno", '1'),
-        ("owt", '2'),
-        ("eerht", '3'),
-        ("ruof", '4'),
-        ("evif", '5'),
-        ("xis", '6'),
-        ("neves", '7'),
-        ("thgie", '8'),
-        ("enin", '9'),
+        ("eno", 1),
+        ("owt", 2),
+        ("eerht", 3),
+        ("ruof", 4),
+        ("evif", 5),
+        ("xis", 6),
+        ("neves", 7),
+        ("thgie", 8),
+        ("enin", 9),
     ];
 
     let char_vec: String = input.chars().rev().collect();
     get_first_number_in_iterator(char_vec.chars(), &number_strings)
 }
 
-fn get_first_number_in_iterator(input: Chars, number_strings: &[(&str, char)]) -> char {
+fn get_first_number_in_iterator(
+    input: Chars,
+    number_strings: &[(&str, i32)],
+) -> Result<i32, io::Error> {
     let mut curr_number_string = "".to_owned();
     for c in input {
         if c.is_numeric() {
-            return c;
+            return Ok(c.to_string().parse().unwrap());
         }
 
         curr_number_string.push(c);
@@ -51,23 +55,19 @@ fn get_first_number_in_iterator(input: Chars, number_strings: &[(&str, char)]) -
 
         for (number_name, number_value) in number_strings {
             if curr_number_string.contains(number_name) {
-                return *number_value;
+                return Ok(*number_value);
             }
         }
     }
 
-    '0'
+    Err(io::Error::new(
+        io::ErrorKind::InvalidData,
+        "Iterator doesn't contain a number.",
+    ))
 }
 
-fn get_calibration_value(input: &str) -> String {
-    let string_literal = [
-        get_first_num_in_string(input),
-        get_last_num_in_string(input),
-    ]
-    .iter()
-    .collect::<String>();
-
-    string_literal
+fn get_calibration_value(input: &str) -> Result<i32, io::Error> {
+    Ok(get_first_num_in_string(input)? * 10 + get_last_num_in_string(input)?)
 }
 
 fn main() -> std::io::Result<()> {
@@ -76,9 +76,8 @@ fn main() -> std::io::Result<()> {
 
     let mut total = 0;
     for line in reader.lines() {
-        let result = get_calibration_value(&line?);
-        let parsed: i32 = result.parse().unwrap();
-        total += parsed;
+        let result = get_calibration_value(&line?)?;
+        total += result;
     }
 
     println!("{}", total);
@@ -92,14 +91,14 @@ mod tests {
     #[test]
     fn verify_solution() {
         let test_cases = [
-            ("two1nine", "29"),
-            ("eighttwothree", "83"),
-            ("abcone2threexyz", "13"),
-            ("xtwoone3four", "24"),
+            ("two1nine", 29),
+            ("eighttwothree", 83),
+            ("abcone2threexyz", 13),
+            ("xtwoone3four", 24),
         ];
 
         for (input, expected_result) in test_cases {
-            let result = get_calibration_value(input);
+            let result = get_calibration_value(input).unwrap();
             assert_eq!(result, expected_result);
         }
     }
