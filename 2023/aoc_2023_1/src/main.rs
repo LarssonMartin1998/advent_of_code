f780f84a900669385ab186d25de38e69acc428a7
Martin Larsson <larssonmartin1998@gmail.com>
2023-12-03 09:48:33 +0100

    Add two star solution for day one, it's got some refactoring to do however

--- 2023/aoc_2023_1/src/main.rs
+++ 2023/aoc_2023_1/src/main.rs
@@ -3,18 +3,56 @@ use std::io::{prelude::*, BufReader};
 use std::str::Chars;
 
 fn get_first_num_in_string(input: &str) -> char {
-    get_first_number_in_iterator(input.chars())
+    let number_strings = [
+        ("one", '1'),
+        ("two", '2'),
+        ("three", '3'),
+        ("four", '4'),
+        ("five", '5'),
+        ("six", '6'),
+        ("seven", '7'),
+        ("eight", '8'),
+        ("nine", '9'),
+    ];
+
+    get_first_number_in_iterator(input.chars(), &number_strings)
 }
 
 fn get_last_num_in_string(input: &str) -> char {
+    let number_strings = [
+        ("eno", '1'),
+        ("owt", '2'),
+        ("eerht", '3'),
+        ("ruof", '4'),
+        ("evif", '5'),
+        ("xis", '6'),
+        ("neves", '7'),
+        ("thgie", '8'),
+        ("enin", '9'),
+    ];
+
     let char_vec: String = input.chars().rev().collect();
-    get_first_number_in_iterator(char_vec.chars())
+    get_first_number_in_iterator(char_vec.chars(), &number_strings)
 }
 
-fn get_first_number_in_iterator(input: Chars) -> char {
+fn get_first_number_in_iterator(input: Chars, number_strings: &[(&str, char)]) -> char {
+    let mut curr_number_string = "".to_owned();
     for c in input {
         if c.is_numeric() {
             return c;
+        }
+
+        curr_number_string.push(c);
+        if curr_number_string.len() > 5 {
+            let mut chars = curr_number_string.chars();
+            chars.next();
+            curr_number_string = chars.as_str().to_string();
+        }
+
+        for (number_name, number_value) in number_strings {
+            if curr_number_string.contains(number_name) {
+                return *number_value;
+            }
         }
     }
 
@@ -54,10 +92,10 @@ mod tests {
     #[test]
     fn verify_solution() {
         let test_cases = [
-            ("1abc2", "12"),
-            ("pqr3stu8vwx", "38"),
-            ("a1b2c3d4e5f", "15"),
-            ("treb7uchet", "77"),
+            ("two1nine", "29"),
+            ("eighttwothree", "83"),
+            ("abcone2threexyz", "13"),
+            ("xtwoone3four", "24"),
         ];
 
         for (input, expected_result) in test_cases {
